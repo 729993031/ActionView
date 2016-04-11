4de9dcf6067b54d235c1ef793c5f83ddba7fae34
Tony <tony@gmail.com>
2016-04-11 23:20:37 +0800

    Initial commit

---
+++ src/com/example/ActionView/MyActivity.java
@@ -0,0 +1,22 @@
+package com.example.ActionView;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MyActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+}
